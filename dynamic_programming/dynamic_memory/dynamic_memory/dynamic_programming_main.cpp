9b9dd40a0836fadb1676f78ac8296bc6a91970aa
Shaw Ken Chang <kenchang408@gmail.com>
2014-03-15 00:30:05 -0700

    added menu prompt and improved the algorithm
    Still do not get correct results yet but added easier to read logics and
    comments for debugging.

--- dynamic_programming/dynamic_memory/dynamic_memory/dynamic_programming_main.cpp
+++ dynamic_programming/dynamic_memory/dynamic_memory/dynamic_programming_main.cpp
@@ -21,22 +21,30 @@ void findBestPC();
 int main ()
 { 
 
-
+double node_num;
+double time; 
+char exit = NULL;
 
 setupInput();
 //displayInput();
-std::cout << "for n0, best (P, C) at time 1 is: " << std::endl;
-findPC_t(n0, 1);
-std::cout << "for n0 best (P, C) at time 2 is: " << std::endl;
-findPC_t(n0, 2);
-std::cout << "for n0, best (P, C) at time 3 is: " << std::endl;
-findPC_t(n0, 3);
-std::cout << "for n0 best (P, C) at time 4 is: " << std::endl;
-findPC_t(n0, 4);
-std::cout << "for n0 best (P, C) at time 5 is: " << std::endl;
-findPC_t(n0, 5);
+std::cout<<"WELCOME.  THIS PROMGRAM WILL CALCULATE B TABLE FOR INDIVIDUAL NODES (WITHOUT COMBINING PARALLEL NODES) \n";
+std::cout<<"---------------------------------------------------------------------------------------------- \n";
 
-  std::cin.get();
+
+while (exit != 'y') {
+std::cout<<"Enter node# (0-3): ";
+std::cin>>node_num;
+std::cout<<"Enter time constraint (integers > 0): ";
+std::cin>>time;
+
+std::cout << "for node" << node_num << ", best (P, C) at time =" << time << " is: " << std::endl;
+findPC_t(n[node_num], time);
+
+std::cout<<"Enter y to exit: ";
+std::cin>>exit;
+
+};
+ 
 
   return 0;
 }
@@ -92,16 +100,13 @@ void findBestPC(){
 	
 	bestPC.clear();
 	
-	
-	int tempIndex = 0;
 	if(bestPC.empty()){
 	//	std::cout << "tuple empty, one tuple added to list" << std::endl;// initial for comparison
 		bestPC.push_back(tuple<double, double> (temp[0].get<0>(), temp[0].get<1>()));//if bestPC is empty, get value of temp at index=0
 		
-	
 	}
 		             
-			tempIndex =1;
+			
 					  double probability, cost;// value for each (P, C) in bestPC list
 						int index = 0;
 						int maxSize = bestPC.size();
@@ -109,70 +114,73 @@ void findBestPC(){
 
 						
 				while(index < maxSize) {
-					 probability = bestPC[index].get<0>();
-						cost = bestPC[index].get<1>();
-					   while(tempIndex<temp.size())
+					
+					
+					   for(int tempIndex=1; tempIndex < temp.size(); tempIndex++)
 					{
-						
-						//std::cout << "tempIndex = " << tempIndex << std::endl;
-						//std::cout << "temp.size() = " << temp.size() << std::endl;
+					
+					
 						probability = bestPC[index].get<0>();
 						cost = bestPC[index].get<1>();
-
-							
+						int ignore = 0;
+						for (int test = 0; (test < bestPC.size()) && (ignore == 0); test++){
+								if ((temp[tempIndex].get<0>() == bestPC[test].get<0>()) && (temp[tempIndex].get<1>() == bestPC[test].get<1>()) ){
+									if (temp[tempIndex].get<0>() <= bestPC[test].get<0>() && bestPC[test].get<1>()) 
+										ignore = 1;
+								}
+							}	
 						
 
-				//	std::cout << " TEMP(" << temp[tempIndex].get<0>() 
-						//<< ", " << temp[tempIndex].get<1>() << ")" << std::endl;
-					//std::cout << " BESTPC(" << probability 
-					//	<< ", " << cost << ")" << std::endl;
-						
+					std::cout << " TEMP(" << temp[tempIndex].get<0>() 
+						<< ", " << temp[tempIndex].get<1>() << ")" << std::endl;
+					std::cout << " BESTPC(" << probability 
+						<< ", " << cost << ")" << std::endl;
+						if (ignore !=1)
+						{
 						if (temp[tempIndex].get<0>() <= probability && temp[tempIndex].get<1>() > cost) 
 						{
-							//std::cout << "temp (P, C) has worse cost and worst and equal probability, do nothing" << std::endl;
+							std::cout << "temp (P, C) has worse cost and worst and equal probability, do nothing" << std::endl;
 						}
-						else if (temp[tempIndex].get<0>() == probability && temp[tempIndex].get<1>() == cost) 
-						{
-							//std::cout << "temp (P, C) has equal cost and probability, do nothing" << std::endl;
-						}
-				
 						else if (temp[tempIndex].get<0>() > probability && temp[tempIndex].get<1>() <= cost)
 						{
-							//std::cout << "temp (P, C) has better or equal cost and  better probability, erase bestPC[bestIndex] and add temp (P,C) to bestPC" << std::endl;
-							
+							std::cout << "temp (P, C) has equal or better cost and  better probability" << std::endl;
+
 							bestPC[index].get<0>() = temp[tempIndex].get<0>();
 							bestPC[index].get<1>() = temp[tempIndex].get<1>();
-							//std::cout << "bestPC[index].get<0>(), bestPC[index].get<1>() = " << bestPC[index].get<0>() << ", " << bestPC[index].get<1>() << std::endl;
-						   // std::cout << "bestPC.size() = " << bestPC.size() << std::endl;
-							
-							
+
 						}
+						else if (temp[tempIndex].get<0>() > probability && temp[tempIndex].get<1>() > cost)
+						{
+							std::cout << "temp (P, C) has worst cost and better probability" << std::endl;
+							bestPC.push_back(tuple<double, double> (temp[tempIndex].get<0>(), temp[tempIndex].get<1>()));	
+						}
+
 						else if (temp[tempIndex].get<0>() == probability && temp[tempIndex].get<1>() < cost)
 						{
-							//std::cout << "temp (P, C) has better cost and equal probability" << std::endl;
+							std::cout << "temp (P, C) has better cost and  equal probability" << std::endl;
 							
 							bestPC[index].get<0>() = temp[tempIndex].get<0>();
 							bestPC[index].get<1>() = temp[tempIndex].get<1>();
+				
 						}
 						else 
 						{
-							//std::cout << "temp (P, C) has better cost but worse probability" << std::endl;
-							bestPC.push_back(tuple<double, double> (temp[tempIndex].get<0>(), temp[tempIndex].get<1>()));
+							std::cout << "temp (P, C) has better cost but worse probability" << std::endl;
+							bestPC.push_back(tuple<double, double> (temp[tempIndex].get<0>(), temp[tempIndex].get<1>()));	
 						}
-							// std::cout << "bestPC.size() = " << bestPC.size() << std::endl;
-						tempIndex ++;
-						
-					}
+						// std::cout << "bestPC.size() = " << bestPC.size() << std::endl;
 					
-						maxSize = bestPC.size();
-						 //std::cout << "maxSize = " << maxSize << std::endl;
+							//std::cout << "tempIndex = " << tempIndex << std::endl;
+						}
+					
+					}
+
+				maxSize = bestPC.size();
+						 std::cout << "maxSize = " << maxSize << std::endl;
 						  
 						index ++;
-						  //std::cout << "index = " << index << std::endl;
-				}
-
-				
-						
+						  std::cout << "index = " << index << std::endl;
+				}	
 					
 	
 
